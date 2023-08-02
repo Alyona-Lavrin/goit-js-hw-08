84fb3107b1a8111fac768f5e927031da633f2947
Alona <polejcukalona@gmail.com>
2023-08-03 00:01:31 +0300

    jhjhjhgj

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.gallery');
+
+  function createGalleryItem(item) {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
+
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.alt = item.description;
+    image.setAttribute('data-source', item.original);
+
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+
+    return galleryItem;
+  }
+
+  galleryItems.forEach(item => {
+    const galleryItem = createGalleryItem(item);
+    gallery.appendChild(galleryItem);
+  });
+
+      var lightbox = new SimpleLightbox('.gallery a', 
+      {
+        captionsData: 'alt',
+        captions: true, 
+        captionDelay: 250,
+    });
+});
