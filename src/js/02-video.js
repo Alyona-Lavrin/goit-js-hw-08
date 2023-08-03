91dbc07039ab5c9ef33d415725cb38ec74dd57b2
Alona <polejcukalona@gmail.com>
2023-08-04 00:05:14 +0300

    dfsfsd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,12 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function() {
-    console.log('played the video!');
+const savedTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(savedTime);
+
+player.on('timeupdate', function(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
 });
 
 player.getVideoTitle().then(function(title) {
