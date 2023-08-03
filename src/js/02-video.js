65a44d8b268b54bcea8a2cfce59f306b4138dd45
Alona <polejcukalona@gmail.com>
2023-08-04 00:23:45 +0300

    iuii

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,3 +10,8 @@ player.setCurrentTime(savedTime ? savedTime : 0);
 player.on('timeupdate', function(data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
 });
+if (!lastUpdateTime || currentTimeInMillis - parseInt(lastUpdateTime, 10) >= 1000) {
+    localStorage.setItem("videoplayer-current-time", currentTime);
+    localStorage.setItem("videoplayer-last-update", currentTimeInMillis.toString());
+  }
+  saveCurrentTimeInLocalStorage(currentTime);
