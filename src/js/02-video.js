0381ceecb2f4a5e4a5978529251d0c5e863d954e
Alona <polejcukalona@gmail.com>
2023-08-04 00:50:06 +0300

    ііі

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,17 +9,12 @@ player.setCurrentTime(savedTime ? savedTime : 0);
 
 player.on('timeupdate', function(data) {
     throttledSave(data.seconds);
-    localStorage.setItem("videoplayer-current-time", data.seconds);
 });
 
 const throttle = require('lodash.throttle');
 
 function saveCurrentTimeInLocalStorage(currentTime) {
-  const currentTimeInMillis = new Date().getTime();
-
-  if (currentTimeInMillis - parseInt(currentTime, 10) >= 1000) {
     localStorage.setItem("videoplayer-current-time", currentTime);
-  }
 }
 
 const throttledSave = throttle(saveCurrentTimeInLocalStorage, 1000);
